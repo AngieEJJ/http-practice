e964e5c12adb7b6760daa4e008f0b9559d194c0e
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-27 16:51:16 +0900

    mainscreen UI 완성

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  hintText: 'Search',
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      // print('tapped!');
+                    },
+                    child: const Icon(
+                      Icons.search,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 32,
+                    mainAxisSpacing: 32,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return AspectRatio(
+                      aspectRatio: 1 / 2,
+                      child: Image.network(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4iuwXdFpW0BYdGdEWsN0YOyeRanfN_0g1FQ&usqp=CAU",
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
