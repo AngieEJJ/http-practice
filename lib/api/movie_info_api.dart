d62edb7c4a45764914426c223ad51d067b752ec9
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-02 00:30:45 +0900

    영화정보 api연습

--- lib/api/movie_info_api.dart
+++ lib/api/movie_info_api.dart
@@ -2,13 +2,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class MovieInfoApi {
-  Future getInfo() async {
+  Future getData() async {
     final response = await http
         .get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
     // print(response.statusCode); // 200
     final jsonResult = jsonDecode(response.body);
     // print(jsonResult); //Map형태
-
     }
-
   }
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:movie_info_app/api/movie_info_api.dart';
 import 'package:movie_info_app/model/movie_info.dart';
-import 'package:movie_info_app/model/photo.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -14,61 +11,38 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final List<MovieInfo> movieInfoList = [];
-  final List<Photo> photoList = [];
-  final MovieInfoApi movieInfoApi = MovieInfoApi(); //(추가) MovieInfoApi 인스턴스생성
+  final MovieInfoApi movieInfoApi = MovieInfoApi();
 
   @override
   void initState() {
-    getMovieInfo(); // (추가) getInfo였으나 새롭게 생긴 getMovieInfo 함수가 이제 실행되어야 하니 바뀜.
-    getPhoto();
+    getMovieInfo();
     super.initState();
   }
 
-  Future getMovieInfo() async { //(추가) getInfo함수를 여기서 만들었으나, 따로 뻈기 때문에 그 함수를 불러올 또 다른 함수 생성.
-    final List<MovieInfo> infoList = await movieInfoApi.getInfo();
+  Future getMovieInfo() async {
+    final List<MovieInfo> infoList = await movieInfoApi.getData(); //getInfo 메서드를 호출하면 그 안에서 jsonResult가 생성되어서, 그 결과를 반환
     setState(() {
-      movieInfoList.addAll(infoList); // (추가) 새롭게 생긴 infoList가 이제 우리가 필요한 데이터이며, 모든 데이터는 movieInfoList에 들어간다.
-    });
-  }
-
-  Future getPhoto() async {
-    final response2 = await http
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-    // print(response2.statusCode); // 200
-    final jsonPhoto = jsonDecode(response2.body);
-
-    setState(() {
-      jsonPhoto.forEach((e) {
-        photoList.add(Photo.fromJson(e));
+      infoList.forEach((e) {
+        movieInfoList.add(MovieInfo.fromJson(e as Map<String, dynamic>)); //(추가) 새롭게 생긴 photoList가 이제 우리가 필요한 데이터이며, 모든 데이터는 photoInfoList에 들어간다.
       });
-    });
-  }
+  });
+        }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('영화정보 api 연습'),
-      ),
-      body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
+        appBar: AppBar(title: const Text('영화정보 api 연습'),
+        ),
+        body: ListView(
+          children: movieInfoList.map((e) => ListTile(
+            title: Text(e.title ?? ''),
+            subtitle: Text(e.id.toString()),
           ),
-          itemCount: movieInfoList.length, // (추가) 우리가 필요한 데이터 리스트가 담겨있음.
-          itemBuilder: (BuildContext context, int index) {
+          ).toList(),
 
-            return Column(
-              children: [
-                Image.network(photoList[index].thumbnailUrl.toString(),
-                    height: 150),
-                Text(
-                  movieInfoList[index].title ?? '',
-                )
-              ],
-            );
-          }),
+
+        )
     );
   }
 }
